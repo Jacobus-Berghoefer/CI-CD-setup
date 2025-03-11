640364ddf1af7e85fb9330d7a08a2e6a3ff628a8
Jacobus <jacobus.berghoefer@gmail.com>
2025-03-11 17:57:02 -0500

    rebuilding just to double check

--- server/dist/routes/index.js
+++ server/dist/routes/index.js
@@ -5,6 +5,10 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import apiRoutes from './api/index.js';
+router.get('/test', (_req, res) => {
+    console.log('✅ Test route hit!');
+    res.json({ message: 'Server is reachable' });
+});
 router.use('/api', apiRoutes);
 // serve up react front-end in production
 router.use((_req, res) => {
