717a6cd9b35c38f990d58b9876003fa411058be4
Jacobus <jacobus.berghoefer@gmail.com>
2025-03-12 20:05:28 -0500

    adjust card format

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -7,7 +7,7 @@ describe('App Load Test', () => {
     it('should start the quiz and display a question', () => {
       cy.visit('/');
       cy.get('button').contains('Start Quiz').click();
-      cy.get('.card.p-4 h2').should('be.visible');
+      cy.get('.card').should('be.visible');
       cy.get('h2').should('not.be.empty');
     });
   });
